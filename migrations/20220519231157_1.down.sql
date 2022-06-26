a81d3b3d8b250f3573e68fe516dee1a0acd6dfbf
Rashaad Akbar <rushda2000@gmail.com>
2022-06-26 12:39:55 +0530

    feat: Update script

--- migrations/20220519231157_1.down.sql
+++ migrations/20220519231157_1.down.sql
@@ -1,8 +1,14 @@
 -- Add migration script here
 
+-- NOTE: The order matters
 DROP TABLE IF EXISTS user_sessions;
 DROP TABLE IF EXISTS posts;
-DROP TABLE IF EXISTS members;
-DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS todos;
-DROP TABLE IF EXISTS guilds;
+DROP TABLE IF EXISTS "message";
+DROP TABLE IF EXISTS member_roles;
+DROP TABLE IF EXISTS channel;
+DROP TABLE IF EXISTS member;
+DROP TABLE IF EXISTS "role";
+DROP TABLE IF EXISTS invite;
+DROP TABLE IF EXISTS guild;
+DROP TABLE IF EXISTS users;
